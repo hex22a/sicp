6705e0b5d40a322a1692ce5685b0f130a2b34dd4
Crash <1903291+hex22a@users.noreply.github.com>
2026-07-14 22:55:54 +0400

    exercise 11 iter

--- chapter1/1.11.scm
+++ chapter1/1.11.scm
@@ -4,4 +4,16 @@
     n
     (+ (fr (- n 1)) (fr (- n 2)) (fr (- n 3)))))
 
-(fr 4)
+(fr 10)
+;230
+
+; Iterative
+(define (fi n)
+         (fi-iter 0 1 2 n))
+
+(define (fi-iter a b c count) 
+  (if (= count 0)
+    a
+    (fi-iter b c (+ a b c) (- count 1) )))
+(fi 10)
+;230
